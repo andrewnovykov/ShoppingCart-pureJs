8a03de8b8a1367f170d7a3468e0a1587a8044f44
papapin <papapin777#gmail.com>
2019-05-14 20:57:48 -0700

    start files

---
+++ public/app.js
@@ -0,0 +1,21 @@
+//variables
+
+const cartBtn = document.querySelector('.cart-btn');
+//console.log(cartBtn);
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContant = document.querySelector('.cart-contant');
+const productsDOM = document.querySelector('.products-center');
+
+
+let cart = [];
+
+//getting the products
+class Products {
+
+}
+
